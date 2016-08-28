d4d5cea7e7f3a3e4b8503bb8b314aa497a59f726
Paola Guarasci <paolaguarasci@gmail.com>
2016-08-28 10:55:17 +0200

    Core calculator ok

--- build/dev/js/script.js
+++ build/dev/js/script.js
@@ -5622,6 +5622,41 @@
     }, {} ],
     2: [ function(require, module, exports) {
         var $ = require("jquery");
+        function calc(value) {
+            var operators = {
+                "+": function(a, b) {
+                    return a + b;
+                },
+                "-": function(a, b) {
+                    return a - b;
+                },
+                "/": function(a, b) {
+                    return a / b;
+                },
+                "*": function(a, b) {
+                    return a * b;
+                }
+            };
+            var buffer = Array.from(value);
+            var num = [];
+            var op = [];
+            for (var i = 0; i < buffer.length; i++) {
+                if (i === 0 || i % 2 === 0) {
+                    num.push(buffer[i]);
+                } else if (i === 1 || i % 2 === 1) {
+                    op.push(buffer[i]);
+                }
+            }
+            var base = num[0];
+            var x = 0;
+            var result = num.reduce(function(a, b) {
+                var temp = operators[op[x]](a, b);
+                x++;
+                return temp;
+            });
+            return result;
+        }
+        alert(calc([ 5, "*", 4, "*", 3, "*", 2, "*", 1 ]));
     }, {
         jquery: 1
     } ]
